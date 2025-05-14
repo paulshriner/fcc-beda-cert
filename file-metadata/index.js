71fa04d127f2851ad3ae70cb0be7ffa106876b28
paulshriner <shrinerpaul@gmail.com>
2025-05-14 14:28:48 -0400

    file-metadata: handle form submission, display file metadata

--- file-metadata/index.js
+++ file-metadata/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +13,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// handles submitting form with uploaded file
+// thanks https://www.npmjs.com/package/multer for multer usage
+app.post('/api/fileanalyse/', upload.single('upfile'), (req, res) => {
+  if (req.file == null) {
+    // user clicked on upload without choosing file
+    res.json({"error": "No file uploaded!"});
+  } else {
+    // file exists, grab necessary information and return to user
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
